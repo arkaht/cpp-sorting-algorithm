00546271d0088c36d8940e845bedb2b821d4ed36
Guthen <114919245+arkaht@users.noreply.github.com>
2023-10-02 19:48:58 +0200

    [added] quick sort algorithm

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 
 #include "sort.hpp"
 
-std::vector<int> list1 { 6, 7, 3, 8, 2, 5, 4, 1 };
-std::vector<int> list2 { 8, 7, 6, 5, 4, 3, 2, 1 };
-std::vector<int> list3 { 1, 2, 3, 4, 5, 6, 7, 8 };
+std::vector<int> best_list { 1, 2, 3, 4, 5, 6, 7, 8 };
+std::vector<int> worst_list { 8, 7, 6, 5, 4, 3, 2, 1 };
+std::vector<int> random_list1 { 6, 7, 3, 8, 2, 5, 4, 1 };
+std::vector<int> random_list2 { 5, 2, 8, 1, 4, 3, 7, 6 };
 
-#define PRINT_SORT( list, sort_func ) \
+#define TEST_SORT( list, sort_func ) \
     std::cout << #list " - not sorted:" << std::endl; \
     print_list( list ); \
     std::cout << #list " - " #sort_func ":" << std::endl; \
@@ -26,15 +27,21 @@ void print_list( const std::vector<int>& list )
 
 int main()
 {
-    //PRINT_SORT( list1, bubble_sort );
-    //PRINT_SORT( list2, bubble_sort );
-    //PRINT_SORT( list3, bubble_sort );
+    //  bubble sort
+    //TEST_SORT( best_list, bubble_sort );
+    //TEST_SORT( worst_list, bubble_sort );
+    //TEST_SORT( random_list1, bubble_sort );
+    //TEST_SORT( random_list2, bubble_sort );
 
-    PRINT_SORT( list1, selection_sort );
-    PRINT_SORT( list2, selection_sort );
-    PRINT_SORT( list3, selection_sort );
+    //  selection sort
+    //TEST_SORT( best_list, selection_sort );
+    //TEST_SORT( worst_list, selection_sort );
+    //TEST_SORT( random_list1, selection_sort );
+    //TEST_SORT( random_list2, selection_sort );
 
-    //PRINT_SORT( list1, quick_sort );
-    //PRINT_SORT( list2, quick_sort );
-    //PRINT_SORT( list3, quick_sort );
+    //  quick sort
+    TEST_SORT( best_list, quick_sort );
+    TEST_SORT( worst_list, quick_sort );
+    TEST_SORT( random_list1, quick_sort );
+    TEST_SORT( random_list2, quick_sort );
 }
--- sort.hpp
+++ sort.hpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <algorithm>
 
+//  bubble sorting:
+//	- best case: O(n)
+//  - worst case: O(n²)
 template <class T>
 void bubble_sort( std::vector<T>& list ) 
 {
@@ -17,8 +20,8 @@ void bubble_sort( std::vector<T>& list )
 			if ( *itr > *next_itr )
 			{
 				printf( "%d > %d: swapping\n", *itr, *next_itr );
-
 				std::iter_swap( itr, next_itr );
+
 				is_sorted = false;
 			}
 		}
@@ -31,6 +34,9 @@ void bubble_sort( std::vector<T>& list )
 	}
 }
 
+//  selection sorting:
+//	- best case: O(n²)
+//  - worst case: O(n²)
 template <class T>
 void selection_sort( std::vector<T>& list )
 {
@@ -40,7 +46,6 @@ void selection_sort( std::vector<T>& list )
 
 		auto itr = list.begin() + i;
 		auto smallest_itr = itr;
-		//bool is_sorted = true;
 
 		//  find smallest value
 		for ( auto next_itr = itr; next_itr != list.end(); next_itr++ )
@@ -55,21 +60,62 @@ void selection_sort( std::vector<T>& list )
 		if ( smallest_itr != itr )
 		{
 			printf( "%d > %d: swapping\n", *itr, *smallest_itr );
-
 			std::iter_swap( itr, smallest_itr );
-			//is_sorted = false;
 		}
-
-		/*if ( is_sorted )
-		{
-			printf( "sorted\n" );
-			break;
-		}*/
 	}
 }
 
+
 template <class T>
-void quick_sort( std::vector<T>& list )
+int quick_sort_partition( std::vector<T>& list, int from_id, int to_id )
 {
-	
+	for ( int i = from_id; i <= to_id; ) 
+	{
+		//  move greater values to the right
+		if ( list[i] > list[to_id] ) 
+		{
+			auto pivot = list.begin() + to_id;
+
+			//  swap previous & pivot only if different from current
+			if ( i != to_id - 1 ) 
+			{
+				std::iter_swap( pivot, pivot - 1 );
+			}
+
+			//  swap current & pivot
+			std::iter_swap( list.begin() + i, pivot );
+
+			to_id--;
+		}
+		//  increase index & stop on indexes equality
+		else if ( i++ == to_id )
+		{
+			//printf( "partition ended at %d\n", to_id );
+			return to_id;
+		}
+	}
+
+	return to_id + 1;
+}
+
+//  quick sorting:
+//  - best case O(n log(n))
+//  - worst case O(n²)
+template <class T>
+void quick_sort( std::vector<T>& list, int from_id = 0, int to_id = -1 )
+{
+	//  auto-fill 'to_id'
+	if ( to_id == -1 ) 
+	{
+		int size = list.size();
+		to_id = size - 1;
+	}
+	if ( to_id <= from_id ) return;
+
+	int pivot_id = quick_sort_partition( list, from_id, to_id );
+	//printf( "%d to %d: %d\n", from_id, to_id, pivot_id );
+
+	//  recursive partitioning
+	quick_sort( list, from_id, pivot_id - 1 );
+	quick_sort( list, pivot_id, to_id );
 }
